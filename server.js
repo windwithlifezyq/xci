3f70cf4dd5930c4521c131be718ac6dca29a9be9
zhangyq <zhangyq@Ctrip.com>
2018-11-18 14:23:18 +0800

    original

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var bodyParser = require('body-parser');
 var express = require('express')
-var app = express()
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
