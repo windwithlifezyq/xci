ff0118160b19bdbbf14cfd9819c0d8360350f8fd
zhangyq <zhangyq@Ctrip.com>
2018-11-18 14:07:59 +0800

    original

---
+++ server.js
@@ -0,0 +1,18 @@
+var bodyparser = require('body-parser');
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send(`
+    <html>
+      <head></head>
+      <body>
+         <h1>hello world<h1>
+      </body>
+    </html>
+  `)
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
