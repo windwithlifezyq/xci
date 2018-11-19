adb5531eaaa5ddd52f86b8c59be4101748ceaf69
379163259@qq.com <ctrip@dst54785.cn1.global.ctrip.com>
2018-11-19 14:43:29 +0800

    add git clone function

--- deploy-project.sh
+++ deploy-project.sh
@@ -6,11 +6,12 @@ echo $WEB_PATH
 TARGET_PATH=${WEB_PATH}
 echo $WEB_PATH
 cd $WEB_PATH
-touch test002.txt
+touch test003.txt
 echo "pulling source code..."
 #git reset --hard origin/master
-#git clone https://github.com/windwithlife/coder.git
-#cd ./coder
+git clone https://github.com/windwithlife/coder.git
+cd ./coder
+touch testx.txt
 #git checkout master
 #npm install
 #npm run dev
