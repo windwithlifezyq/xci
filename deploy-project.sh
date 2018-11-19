098ce837b7f7ea34de33dfb10523d8e61c80cb51
ctrip <ctrip@dst54785.cn1.global.ctrip.com>
2018-11-19 20:32:07 +0800

    test

--- deploy-project.sh
+++ deploy-project.sh
@@ -2,22 +2,17 @@
 
 echo "Start deployment"
 WEB_PATH=${PWD}/../autoRelease
-echo $WEB_PATH
-TARGET_PATH=${WEB_PATH}
-echo $WEB_PATH
 cd $WEB_PATH
-touch test003.txt
 echo "pulling source code..."
 #git reset --hard origin/master
-git clone https://github.com/windwithlife/coder.git
+#git clone https://github.com/windwithlife/coder.git
 cd ./coder
-touch testxxx.txt
 #git checkout master
 npm install
 kill -9 $(lsof -i:3000 |awk '{print $2}' | tail -n 2)
 npm run devx
-######npm run start
-echo "Finished."
+touch .git/cloneflag.flag
+echo "Finished!!!!."
 
 
 
