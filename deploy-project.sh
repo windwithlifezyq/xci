44e97157e72ba4d3de73db51962579ddc2bed460
ctrip <ctrip@dst54785.cn1.global.ctrip.com>
2018-11-20 11:44:34 +0800

    release java

--- deploy-project.sh
+++ deploy-project.sh
@@ -11,10 +11,19 @@ git pull
 
 #npm install
 #kill -9 $(lsof -i:3000 |awk '{print $2}' | tail -n 2)
+
+
+docker build -f files/server/simpleserver/Dockerfile -t release:java .
+docker stop test_java_server
+docker rm test_java_server
+docker run -d --name test_java_server -p 8080:8080 release:java
+
+
+
 docker build -f ./Dockerfile -t release:node .
 docker stop test_node
 docker rm test_node
-docker run --name test_node -p 3000:3000 release:node
+docker run -d --name test_node -p 3000:3000 release:node
 echo "Finished!!!!."
 
 
