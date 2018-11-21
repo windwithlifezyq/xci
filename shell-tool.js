f16270b258054c0d2824095875c7f247d0a944a7
ctrip <ctrip@dst54785.cn1.global.ctrip.com>
2018-11-21 16:22:57 +0800

    add git clone function

--- shell-tool.js
+++ shell-tool.js
@@ -4,14 +4,6 @@
 
 require('shelljs/global');
 
-var RELEASE_BASE_PATH = "../autoRelease/";
-
-
-function test() {
-    var checkDir = fs.existsSync('../autoRele/');
-    console.log(checkDir);
-}
-
 
 function restartNPM (port){
     console.log( "Start restart  server myself")
@@ -34,11 +26,8 @@ function restartNPM (port){
 function installNPMPackages (){
     console.log( "Start install NPM relatived packages!")
     let installPackages = 'npm install';
-    let killServer = "kill -9 $(lsof -i:" + port + "|awk '{print $2}' | tail -n 2)";
-    let startNPM = 'npm start';
-    exec(installPackages);
-    exec(killServer);
-    if (exec(startNPM).code !== 0) {
+
+    if (exec(installPackages).code !== 0) {
         console.log('Error: failed to install NPM relatived packages');
         return false;
     }else{
@@ -50,4 +39,5 @@ function installNPMPackages (){
 
 module.exports = {
     restartNPM: restartNPM,
-    installPackages:installNPMPackages,
+    installPackages: installNPMPackages
+}
