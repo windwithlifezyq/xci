6990608c1b280fdab7bb5eee7bb8bfba7e95d37f
ctrip <ctrip@LPT40202.local>
2019-01-01 19:51:26 +0800

    add build image for k8s

---
+++ libs/docker-tool.js
@@ -0,0 +1,188 @@
+/**
+ * Created by Joe on 2018/11/21.
+ */
+
+require('shelljs/global');
+yaml = require('node-yaml');
+var fs = require('fs');
+var path = require('path');
+var RELEASE_BASE_PATH = "../autoRelease/";
+var RELEASE_DOCKERFILE_PATH = path.join(process.cwd(),"./cloud-resources/dockerfiles/");
+var K8S_RESOURCE_TEMPLATE_PATH = path.join(process.cwd(),'./cloud-resources/k8s/templates/');
+var K8S_RESOURCE_DEPLOYMENT_PATH = path.join(process.cwd(),'./cloud-resources/k8s/resources/deployments/');
+
+function test() {
+    var checkDir = fs.existsSync('../autoRele/');
+    console.log(checkDir);
+}
+
+function buildDockerImage(dockerName,envName,dockerfilePath) {
+    if (dockerfilePath){
+        cd(dockerfilePath)
+    }
+
+    //var isExist = fs.existsSync(release_directory);
+
+    //if (isExist) {
+        //cd(release_directory);
+        let imageName = dockerName + ":" + envName
+        let buldCommand = 'docker build . -f ./Dockerfile -t ' + imageName ;
+        if (exec(buldCommand).code !== 0) {
+            console.log('failed to build docker image:' + imageName)
+        }else{
+            console.log('sucessful to build image');
+        }
+        //exec(gitResetCommand);
+    //} else {
+     //  console.log('Could not find source code!!!');
+    //}
+
+
+}
+function getDockerFileByParams(lang,type) {
+    let dockerFile = 'Dockerfile' + '-' + lang + '-' + type;
+    return dockerFile;
+}
+function buildServiceDockerImage(name,label,lang,type,dockerfilePath) {
+    if (dockerfilePath){
+        cd(dockerfilePath)
+    }
+    let dockerFileName = getDockerFileByParams(lang,type);
+    let imageName  = getDockerImageName(name,label,type);
+
+    let dockerFileSource = RELEASE_DOCKERFILE_PATH + dockerFileName;
+    let dockerFileDestPath = './' ;
+    cp(dockerFileSource,dockerFileDestPath);
+
+    let buldCommand = 'docker build . -f ./'+ dockerFileName +  ' -t ' + imageName ;
+    console.log('Docker image build command:' + buldCommand)
+    console.log('Docker image build env:' + process.cwd());
+    if (exec(buldCommand).code !== 0) {
+        console.log('failed to build docker image:' + imageName)
+    }else{
+        console.log('sucessful to build image');
+    }
+}
+function runDockerImage(dockerName,envName,mapPort){
+    let imageName = dockerName + ":" + envName;
+    let containerName = dockerName +"_" +  envName;
+    let stopContainerCommand = 'docker stop ' + containerName;
+    let removeContainerCommand = 'docker rm ' + containerName;
+    let runCommand = 'docker run -d  --name=' +  containerName + ' -p ' + mapPort + ":" + mapPort + "  " + imageName;
+
+    console.log(stopContainerCommand);
+    console.log(removeContainerCommand);
+    console.log(runCommand);
+    exec(stopContainerCommand);
+    exec(removeContainerCommand);
+    if (exec(runCommand).code !== 0){
+        console.log('failed to run container based imageName:' + imageName);
+    }
+
+}
+
+
+function buildAndRun(dockerName,envName,path,port){
+
+    buildDockerImage(dockerName,envName,path);
+    runDockerImage(dockerName,envName,port);
+
+}
+function getServiceName(serviceName, type){
+    let imageName = "a/b:1.0.1";
+    if (type){
+        imageName = serviceName +'-' + type;
+    }else{
+        imageName = serviceName ;
+    }
+    return imageName;
+}
+function getDockerImageName(serviceName,labelName, type){
+    let imageName = "a/b:1.0.1";
+    imageName = getServiceName(serviceName,type) + ":" + labelName;
+    return imageName;
+}
+function createK8sOperationFiles(serviceName,imageName){
+
+    let currentPath = process.cwd();
+    console.log(currentPath);
+    let templateFilePath = path.join(currentPath,'./cloud-resources/k8s/templates/');
+
+    let deploymentTemplate = templateFilePath + 'deployment.yaml';
+    let serviceTemplate = templateFilePath + 'service.yaml';
+
+    let deployServiceFile = path.join(currentPath,"./cloud-resources/k8s/resources/deployments/") +  serviceName;
+
+    let tempDeployFile = deployServiceFile + "-deploy.yaml"
+    let tempServiceFile = deployServiceFile + "-service.yaml"
+
+    let finalDeploymentFileName = deployServiceFile +'-deployment.yaml';
+    console.log("deploy template" + deploymentTemplate);
+
+
+
+    let deploy = yaml.readSync(deploymentTemplate, {encoding: "utf8",schema: yaml.schema.defaultSafe})
+    deploy.metadata.name = serviceName;
+    deploy.metadata.labels.k8sApp = serviceName;
+    deploy.spec.selector.k8sApp = serviceName;
+    deploy.spec.template.metadata.name= serviceName;
+    deploy.spec.template.metadata.labels.k8sApp= serviceName;
+    deploy.spec.template.spec.containers[0].image= imageName;
+    console.log(JSON.stringify(deploy));
+    yaml.writeSync(tempDeployFile,deploy,"utf8");
+
+    console.log("service template" + serviceTemplate);
+    let service = yaml.readSync(serviceTemplate, {encoding: "utf8",schema: yaml.schema.defaultSafe})
+    service.metadata.name = serviceName;
+    service.metadata.labels.k8sApp = serviceName;
+    service.spec.selector.k8sApp = serviceName;
+    console.log(deploy);
+    yaml.writeSync(tempServiceFile,service,"utf8");
+
+    var contentTextDeployment = fs.readFileSync(tempDeployFile,'utf-8');
+    var contentTextService = fs.readFileSync(tempServiceFile,'utf-8');
+    var contentSplitLine = "--------------\r\n"
+
+    rm(finalDeploymentFileName);
+    rm(tempDeployFile);
+    rm(tempServiceFile);
+
+    var result = fs.appendFileSync(finalDeploymentFileName,contentTextDeployment);
+    result = fs.appendFileSync(finalDeploymentFileName,contentSplitLine);
+    result = fs.appendFileSync(finalDeploymentFileName,contentTextService);
+
+    console.log("DockerImageName:" + imageName);
+    return imageName;
+}
+function releaseService2Cloud(serviceName,imageName){
+
+    let currentPath = process.cwd();
+    console.log(currentPath);
+    let deployServicePath = path.join(currentPath,"./cloud-resources/k8s/resources/deployments/");
+    let finalDeploymentFileName = deployServicePath + serviceName +'-deployment.yaml';
+
+    let runUnDeployCommand = 'kubectl delete -f  ' + finalDeploymentFileName;
+    let runDeployCommand = 'kubectl create -f  ' + finalDeploymentFileName;
+
+    console.log("Exec Command String:" + runDeployCommand);
+    exec(runUnDeployCommand);
+    if(exec(runDeployCommand).code !== 0){
+        console.log('failed to release service to k8s based imageName:' + imageName);
+    }else{
+        console.log('sucessfule to release service to k8s cloud platform!')
+    }
+
+}
+
+function release2K8sCloud(name,labelName,type) {
+    let imageName = getDockerImageName(name,labelName,type);
+    let serviceName  = getServiceName(name,labelName, type);
+    createK8sOperationFiles(serviceName,imageName);
+    releaseService2Cloud(serviceName,imageName);
+}
+
+module.exports = {
+    buildImage: buildDockerImage,
+    buildServiceDockerImage:buildServiceDockerImage,
+    release2K8sCloud:release2K8sCloud
+}
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var bodyParser = require('body-parser');
 var express = require('express')
 var fs= require('fs')
-var gitTools = require('./git-tool');
-var dockerTools = require('./docker-tool');
-var shellTools  = require('./shell-tool');
+var gitTools = require('./libs/git-tool');
+var dockerTools = require('./libs/docker-tool');
+var shellTools  = require('./libs/shell-tool');
 
 var app = express();
 console.log(new Date().toLocaleString());
+
+class Test{
+    myfunc(){
+        console.log('in myfunc');
+    }
+}
+let cls = Test;
+let obj = new Test();
+console.log('class name:' + cls.name + 'obj name:' + obj.name + obj.myfunc.name);
 
 function run_cmd(cmd, args, callback) {
     var spawn = require('child_process').spawn;
@@ -44,7 +53,18 @@ app.post('/gitPushEventProject/:serverPort',function(req, res){
     console.log('current directory is:' + process.cwd());
 
     let originDirectory = process.cwd();
-    var params = {name:"coder"};
+    //let targetPath = './';
+    var params = {targetPath:'./',name:"coder",lang:'java',type:'soa',label:'latest',cloneUrl:'test'};
+    if(req.query.targetPath) {
+        params.targetPath = req.query.targetPath;
+    }
+    if(req.query.lang) {
+        params.lang = req.query.lang;
+    }
+    if(req.query.type) {
+        params.type = req.query.type;
+    }
+
     if (req.body.repository){
         params.name = req.body.repository.name;
         params.gitUrl = req.body.repository.git_url;
@@ -57,10 +77,11 @@ app.post('/gitPushEventProject/:serverPort',function(req, res){
     if (result){
         let envName = "dev";
         if(params.name =='coder'){
-            dockerTools.makeImageAndRun(params.name + "_web",envName,"",3000);
-            dockerTools.makeImageAndRun(params.name + "_server",envName,"./files/server/simpleserver/",8080);
+            //dockerTools.makeImageAndRun(params.name + "_web",envName,"",3000);
+            //dockerTools.makeImageAndRun(params.name + "_server",envName,"./files/server/simpleserver/",8080);
+            dockerTools.buildServiceDockerImage(params.name,params.label,params.lang,params.type,"./files/server/simpleserver/");
         }else{
-            dockerTools.makeImageAndRun(params.name,envName,"./",serverPort);
+            //dockerTools.makeImageAndRun(params.name,envName,"./",serverPort);
         }
     }else{
         console.log('failed to process release,root case: git fetch a failure!')
@@ -75,6 +96,11 @@ app.get('/', function (req, res) {
   res.send('Hello,world!')
 })
 
+
+app.get('/test', function (req, res) {
+    dockerTools.release2K8sCloud('test','fat','soa');
+    res.send('Hello,world! test!');
+})
 process.on('uncaughtException', function (err) {
     //打印出错误
     console.log(err);
