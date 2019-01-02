76503ea8a689fd48afd48f384a5fa6c8e47e1a25
ctrip <ctrip@LPT40202.local>
2019-01-02 10:35:55 +0800

    add release to k8s cloud function

--- libs/shell-tool.js
+++ libs/shell-tool.js
@@ -7,12 +7,7 @@ require('shelljs/global');
 
 function restartNPM (port){
     console.log( "Start restart  server myself")
-
-    let installPackages = 'npm install';
-    let killServer = "kill -9 $(lsof -i:" + port + "|awk '{print $2}' | tail -n 2)";
-    let startNPM = 'npm start';
-    exec(installPackages);
-    exec(killServer);
+    let startNPM = "kill -9 $(lsof -i:" + port + "|awk '{print $2}' | tail -n 2) && npm run start";
     if (exec(startNPM).code !== 0) {
         console.log('Error: failed to restart server');
         return false;
@@ -20,10 +15,11 @@ function restartNPM (port){
         console.log('successful to restart server');
         return true;
     }
-
 }
 
 function restartServer (){
+    restartNPM(3333);
+    return;
     console.log( "Restarting  server myself");
     let startCommand = 'npm restart start';
     if (exec(startCommand).code !== 0) {
